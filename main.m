f4aefa4eff81dd3c172d26ba0972e39b150a6cdd
Abner <abnersousanascimento@gmail.com>
2019-05-01 17:43:26 -0300

    fix

--- main.m
+++ main.m
@@ -10,17 +10,25 @@
 clear; close all; clc;
 
 CLASSES = 10;
-ITERACOES = 50;
+ITERACOES = 2;
 CONSTANTE = 1;
 KERNEL = 'gaussian';
 
 % Leitura dos dados
-data = [csvread('training.csv')];
+data = csvread('training.csv');
 
 all_features = data(:, 1:64);
-all_classes = data(:, 65);
+all_classes = data(:, 65) + 1; % Soma 1 para ajustar aos indices do MATLAB.
 
+% Particionamento Hold-Out
 p = cvpartition(all_classes, 'HoldOut');
+
+% Vetor com os erros de cada iteração
+errors = zeros(1, ITERACOES);
+
+% Array de matrizes com o resultado esperado de cada iteração (primeira
+% coluna), ao lado do resultado obtido (segunda coluna).
+results = zeros(p.TestSize, 2, ITERACOES);
 
 for i=1:ITERACOES
     fprintf('Iteracao %d\n', i);
@@ -33,21 +41,21 @@ for i=1:ITERACOES
     %% Treina array de modelos SVM, um para cada classe
     models = {};
     for j = 1:CLASSES
-        c = uint8(train_classes == j - 1);
+        c = uint8(train_classes == j);
         m = fitcsvm(train_features, c, 'KernelFunction', KERNEL, 'BoxConstraint', CONSTANTE);
         models{j} = m;
-        fprintf('- Classe %d\n', j - 1);
+        fprintf('- Classe %d\n', j);
     end
     
     %% Separa amostras de teste
     test_idx = test(p);
     test_features = all_features(test_idx, :);
-    test_classes = all_classes(test_idx);
+    expected_output = all_classes(test_idx);
     
     %% Calcula predições
     fprintf('Calculando predições...\n');
-    predictions = zeros(size(test_classes));
-    for k=1:size(test_features, 1)
+    predictions = zeros(1, p.TestSize);
+    for k=1:p.TestSize
         sample = test_features(k, :);
         
         % Array com as predições dos modelos
@@ -61,20 +69,28 @@ for i=1:ITERACOES
         
         % O modelo com a maior predição é o escolhido
         [~, predicted_label] = max(model_predictions);
-        predictions(k) = predicted_label - 1;
+        predictions(k) = predicted_label;
     end
     
-    %% Plota matriz de confusão
-    targets = zeros(CLASSES, length(test_classes));
-    outputs = zeros(CLASSES, length(predictions));
-    subs = 1:length(test_classes);
-    targets_idx = sub2ind(size(targets), test_classes + 1, subs');
-    outputs_idx = sub2ind(size(outputs), predictions + 1, subs');
-    targets(targets_idx) = 1;
-    outputs(outputs_idx) = 1;
-    
-    plotconfusion(targets, outputs);
+    results(:, 1, i) = expected_output;
+    results(:, 2, i) = predictions;
+    errors(i) = sum(uint8(predictions == expected_output'));
     
     %% Reparticiona para próximo teste
     p = repartition(p);
 end
+
+%% Plota matriz de confusão da N-ésima iteração.
+N = 1;
+r = results(:, :, N);
+targets = zeros(CLASSES, p.TestSize);
+outputs = zeros(CLASSES, p.TestSize);
+subs = 1:p.TestSize;
+targets_idx = sub2ind(size(targets), r(:, 1), subs');
+outputs_idx = sub2ind(size(outputs), r(:, 2), subs');
+targets(targets_idx) = 1;
+outputs(outputs_idx) = 1;
+plotconfusion(targets, outputs);
+
+%% Plota erros
+plot(1:ITERACOES, errors);
