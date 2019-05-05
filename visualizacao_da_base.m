ac4ec363d85ce5d0d5544a03bbb31758d329ed53
Lucas Santos <lucas26xd@gmail.com>
2019-05-05 15:37:47 -0300

    Exibe as imagens dos números em ordem crescente e aguarda o fechamento da tela anterior

--- visualizacao_da_base.m
+++ visualizacao_da_base.m
@@ -1,16 +1,24 @@
 clear; close all; clc;
-QTD = 4;
+CLASSES = 10;
 
 data = csvread('training.csv');
-r = randi(length(data), QTD, 1);
+base = {};
+for i = 1:CLASSES
+    base{i} = data(data(:, 65) == (i-1), :);
+end
 
-for j=1:QTD
+r = randi(length(base{:, 1}), CLASSES, 1);
+
+for i=1:CLASSES
     img = zeros(8);
-    for i=1:8:63
-        img(floor(i / 8) + 1, :) = data(r(j), i:i+7);
+    for j=1:8:63
+        img(floor(j / 8) + 1, :) = base{i}(r(i), j:j+7);
     end
     figure;
     %img = 2 * img / max(max(img));%Descomente para binarizar as imagens
-    image(img);
-    title("Classe = " + data(r(j), 65));
+    img = image(img);
+    title("Classe = " + base{i}(r(i), 65));
+    
+    waitfor(img);
+    %waitforbuttonpress
 end
